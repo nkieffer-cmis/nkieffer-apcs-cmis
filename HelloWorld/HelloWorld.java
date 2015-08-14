556bd6bd66099bd4e0d006446b77791f75a4cb6f
Nathan Kieffer <nathankieffer@gmail.com>
2015-08-14 14:31:38 +0700

    Created HelloWorld

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+    public static void main ( String argv[] )
+    {
+        System.out.println("Hey!");
+    }
+}
